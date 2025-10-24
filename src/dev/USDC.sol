495912a8dfb585dbcdeed0290e1c26578361975f
polylend-dev <polylend@tuta.com>
2025-10-24 16:52:35 +0400

    change token name & symbol

--- src/dev/USDC.sol
+++ src/dev/USDC.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.30;
 
 import {ERC20} from "../../lib/solady/src/tokens/ERC20.sol";
 
-contract USDC is ERC20 {
+contract pfUSDC is ERC20 {
     function name() public pure override returns (string memory) {
-        return "USDC";
+        return "PolyLendFaucetUSDC";
     }
 
     function symbol() public pure override returns (string memory) {
-        return "USDC";
+        return "pfUSDC";
     }
 
     function decimals() public pure override returns (uint8) {
