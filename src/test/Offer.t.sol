2111fcc8fc156b199ffe31b599a59380ff0558cd
polylend-dev <polylend@tuta.com>
2025-11-25 17:25:55 +0400

    fix all tests

--- src/test/Offer.t.sol
+++ src/test/Offer.t.sol
@@ -50,72 +50,92 @@ contract PolyLendOfferTest is PolyLendTestHelper {
         assertEq(polyLend.nextOfferId(), 1);
     }
 
-    // function test_revert_PolyLendOfferTest_offer_InsufficientFunds(
-    //     uint128 _collateralAmount,
-    //     uint128 _loanAmount,
-    //     uint256 _rate,
-    //     uint32 _minimumDuration,
-    //     uint128 _balance
-    // ) public {
-    //     _setUp(_collateralAmount, _loanAmount, _rate, _minimumDuration);
+    function test_revert_PolyLendOfferTest_offer_InsufficientFunds(
+        uint128 _collateralAmount,
+        uint128 _loanAmount,
+        uint256 _rate,
+        uint256 _minimumLoanAmount,
+        uint256 _duration,
+        uint32 _minimumDuration,
+        uint128 _balance
+    ) public {
+        _setUp(_collateralAmount, _rate);
 
-    //     uint256 balance = bound(_balance, 0, _loanAmount - 1);
-    //     vm.startPrank(lender);
-    //     usdc.mint(lender, balance);
-    //     vm.expectRevert(InsufficientFunds.selector);
-    //     polyLend.offer(requestId, _loanAmount, rate);
-    //     vm.stopPrank();
-    // }
+        uint256 balance = bound(_balance, 0, _loanAmount - 1);
+        vm.startPrank(lender);
+        usdc.mint(lender, balance);
+        vm.expectRevert(InsufficientFunds.selector);
+        uint256[] memory positionIds = new uint256[](2);
+        positionIds[0] = positionId0;
+        positionIds[1] = positionId1;
+        polyLend.offer(_loanAmount, rate, positionIds, _collateralAmount, _minimumLoanAmount, _duration, false);
+        vm.stopPrank();
+    }
 
-    // function test_revert_PolyLendOfferTest_offer_InsufficientAllowance(
-    //     uint128 _collateralAmount,
-    //     uint128 _loanAmount,
-    //     uint256 _rate,
-    //     uint32 _minimumDuration,
-    //     uint128 _allowance
-    // ) public {
-    //     _setUp(_collateralAmount, _loanAmount, _rate, _minimumDuration);
+    function test_revert_PolyLendOfferTest_offer_InsufficientAllowance(
+        uint128 _collateralAmount,
+        uint128 _loanAmount,
+        uint256 _rate,
+        uint256 _minimumLoanAmount,
+        uint256 _duration,
+        uint32 _minimumDuration,
+        uint128 _allowance
+    ) public {
+        _setUp(_collateralAmount, _rate);
 
-    //     uint256 allowance = bound(_allowance, 0, _loanAmount - 1);
-    //     vm.startPrank(lender);
-    //     usdc.mint(lender, _loanAmount);
-    //     usdc.approve(address(polyLend), allowance);
-    //     vm.expectRevert(InsufficientAllowance.selector);
-    //     polyLend.offer(requestId, _loanAmount, rate);
-    //     vm.stopPrank();
-    // }
+        uint256 allowance = bound(_allowance, 0, _loanAmount - 1);
+        vm.startPrank(lender);
+        usdc.mint(lender, _loanAmount);
+        usdc.approve(address(polyLend), allowance);
+        vm.expectRevert(InsufficientAllowance.selector);
+        uint256[] memory positionIds = new uint256[](2);
+        positionIds[0] = positionId0;
+        positionIds[1] = positionId1;
+        polyLend.offer(_loanAmount, rate, positionIds, _collateralAmount, _minimumLoanAmount, _duration, false);
+        vm.stopPrank();
+    }
 
-    // function test_revert_PolyLendOfferTest_offer_InvalidRate_tooLow(
-    //     uint128 _collateralAmount,
-    //     uint128 _loanAmount,
-    //     uint256 _rate,
-    //     uint32 _minimumDuration
-    // ) public {
-    //     _setUp(_collateralAmount, _loanAmount, _rate, _minimumDuration);
+    function test_revert_PolyLendOfferTest_offer_InvalidRate_tooLow(
+        uint128 _collateralAmount,
+        uint128 _loanAmount,
+        uint256 _rate,
+        uint256 _minimumLoanAmount,
+        uint256 _duration,
+        uint32 _minimumDuration
+    ) public {
+        _setUp(_collateralAmount, _rate);
 
-    //     rate = bound(_rate, 0, 10 ** 18);
-    //     vm.startPrank(lender);
-    //     usdc.mint(lender, _loanAmount);
-    //     usdc.approve(address(polyLend), _loanAmount);
-    //     vm.expectRevert(InvalidRate.selector);
-    //     polyLend.offer(requestId, _loanAmount, rate);
-    //     vm.stopPrank();
-    // }
+        rate = bound(_rate, 0, 10 ** 18);
+        vm.startPrank(lender);
+        usdc.mint(lender, _loanAmount);
+        usdc.approve(address(polyLend), _loanAmount);
+        vm.expectRevert(InvalidRate.selector);
+        uint256[] memory positionIds = new uint256[](2);
+        positionIds[0] = positionId0;
+        positionIds[1] = positionId1;
+        polyLend.offer(_loanAmount, rate, positionIds, _collateralAmount, _minimumLoanAmount, _duration, false);
+        vm.stopPrank();
+    }
 
-    // function test_revert_PolyLendOfferTest_offer_InvalidRate_tooHigh(
-    //     uint128 _collateralAmount,
-    //     uint128 _loanAmount,
-    //     uint256 _rate,
-    //     uint32 _minimumDuration
-    // ) public {
-    //     _setUp(_collateralAmount, _loanAmount, _rate, _minimumDuration);
+    function test_revert_PolyLendOfferTest_offer_InvalidRate_tooHigh(
+        uint128 _collateralAmount,
+        uint128 _loanAmount,
+        uint256 _rate,
+        uint256 _minimumLoanAmount,
+        uint256 _duration,
+        uint32 _minimumDuration
+    ) public {
+        _setUp(_collateralAmount, _rate);
 
-    //     rate = bound(_rate, polyLend.MAX_INTEREST() + 1, type(uint64).max);
-    //     vm.startPrank(lender);
-    //     usdc.mint(lender, _loanAmount);
-    //     usdc.approve(address(polyLend), _loanAmount);
-    //     vm.expectRevert(InvalidRate.selector);
-    //     polyLend.offer(requestId, _loanAmount, rate);
-    //     vm.stopPrank();
-    // }
+        rate = bound(_rate, polyLend.MAX_INTEREST() + 1, type(uint64).max);
+        vm.startPrank(lender);
+        usdc.mint(lender, _loanAmount);
+        usdc.approve(address(polyLend), _loanAmount);
+        vm.expectRevert(InvalidRate.selector);
+        uint256[] memory positionIds = new uint256[](2);
+        positionIds[0] = positionId0;
+        positionIds[1] = positionId1;
+        polyLend.offer(_loanAmount, rate, positionIds, _collateralAmount, _minimumLoanAmount, _duration, false);
+        vm.stopPrank();
+    }
 }
